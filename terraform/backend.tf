b8f3538b32541b11b331760d446d3bef0e82bfd1
wangzitian0 <wangzitian0@gmail.com>
2025-12-04 15:09:45 +0800

    fix: add skip_s3_checksum for R2 compatibility (#7)

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -17,6 +17,7 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
     use_path_style              = true
   }
 }
