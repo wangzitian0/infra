250a1cec3ed4cffaaeb72f13dd4628fbe5da9123
Zitian Wang <wangzitian0@gmail.com>
2025-12-12 02:07:56 +0800

    fix: switch kubectl provider to resolve 504 errors

--- 2.platform/versions.tf
+++ 2.platform/versions.tf
@@ -11,8 +11,8 @@ terraform {
       version = "~> 2.0"
     }
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
     }
     helm = {
       source  = "hashicorp/helm"
