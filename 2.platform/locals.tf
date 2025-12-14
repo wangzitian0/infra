9f501cb628728a1cdbf18212b9f28410f9841b1c
Zitian Wang <wangzitian0@gmail.com>
2025-12-15 04:53:59 +0800

    test: trigger CI with minor formatting change

--- 2.platform/locals.tf
+++ 2.platform/locals.tf
@@ -1,7 +1,6 @@
 locals {
   internal_domain         = var.internal_domain != "" ? var.internal_domain : var.base_domain
   portal_sso_gate_enabled = var.enable_portal_sso_gate
-
   # Vault resources only created when vault_root_token is provided
   # This allows CI to skip Vault config when Vault is unreachable
   # Use nonsensitive() to avoid tainting outputs as sensitive
