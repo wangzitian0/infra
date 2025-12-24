f6419d46779742f08a972ad54a3b60ad58704da4
Zitian Wang <wangzitian0@gmail.com>
2025-12-24 18:01:31 +0800

    chore: remove Atlantis deployment in favor of Digger
    BREAKING CHANGE: This removes the Atlantis Helm release from the cluster.
    
    Removed files:
    - bootstrap/2.atlantis.tf (Helm release, RBAC)
    - bootstrap/test_atlantis.tf (test resource)
    - atlantis.yaml (repo config)
    
    Atlantis has been replaced by Digger for Terraform CI/CD.
    The Helm release will be destroyed when this PR is applied.

--- bootstrap/test_atlantis.tf
+++
@@ -1,9 +0,0 @@
-# Test resource for Atlantis verification
-# This file exists to verify Atlantis is working correctly.
-# Verification timestamp: 2025-12-06
-resource "null_resource" "atlantis_test" {
-  triggers = {
-    # Change this value to trigger Atlantis plan
-    test_trigger = "atlantis-bot-verification-v2"
-  }
-}
